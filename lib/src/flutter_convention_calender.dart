54ccd8123f4c7d872bdbcf8c7d5f40121c841254
madhusudhan <ghimire.madhusudhan@cloco.net>
2024-09-04 10:41:01 +0545

    removed animation controller

--- lib/src/flutter_convention_calender.dart
+++ lib/src/flutter_convention_calender.dart
@@ -29,15 +29,9 @@ class ConventionCalendar extends StatefulWidget {
 
 class ConventionCalendarState extends State<ConventionCalendar>
     with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 1000),
-      vsync: this,
-    );
   }
 
   @override
@@ -45,95 +39,89 @@ class ConventionCalendarState extends State<ConventionCalendar>
     return Material(
       color: Colors.white,
       child: SafeArea(
-        child: AnimatedBuilder(
-            animation: _animationController,
-            builder: (context, child) {
-              return TableCalendar(
-                firstDay: widget.firstDay,
-                lastDay: widget.lastDay,
-                focusedDay: widget.focusedDay,
-                onDaySelected: widget.onDaySelected,
-                calendarStyle: widget.calendarStyle ??
-                    CalendarStyle(
-                      defaultTextStyle: const TextStyle(
-                        fontSize: 18,
+        child: TableCalendar(
+          firstDay: widget.firstDay,
+          lastDay: widget.lastDay,
+          focusedDay: widget.focusedDay,
+          onDaySelected: widget.onDaySelected,
+          calendarStyle: widget.calendarStyle ??
+              CalendarStyle(
+                defaultTextStyle: const TextStyle(
+                  fontSize: 18,
+                ),
+                weekendTextStyle: const TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                ),
+                defaultDecoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    borderRadius: BorderRadius.circular(8)),
+                weekendDecoration: BoxDecoration(
+                  color: Colors.red.shade100,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                todayDecoration: const BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+                selectedDecoration: const BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+                selectedTextStyle: const TextStyle(color: Colors.white),
+              ),
+          daysOfWeekHeight: 40,
+          headerStyle: widget.headerStyle ??
+              HeaderStyle(
+                titleCentered: true,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 4.0,
+                        spreadRadius: 2.0,
+                        color: Colors.grey.shade200,
+                      )
+                    ]),
+                headerMargin:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                formatButtonVisible: false,
+                titleTextStyle: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+                leftChevronIcon: const Icon(Icons.arrow_back_ios_new_rounded,
+                    color: Colors.black),
+                rightChevronIcon: const Icon(Icons.arrow_forward_ios_rounded,
+                    color: Colors.black),
+              ),
+          calendarBuilders: widget.calendarBuilders ??
+              CalendarBuilders(
+                dowBuilder: (context, day) {
+                  final text = DateFormat.E().format(day);
+                  if (day.weekday == DateTime.saturday ||
+                      day.weekday == DateTime.sunday) {
+                    return Center(
+                      child: Text(
+                        text,
+                        style: const TextStyle(
+                            color: Color(0xFFE60026), fontSize: 18),
                       ),
-                      weekendTextStyle: const TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
-                      ),
-                      defaultDecoration: BoxDecoration(
-                          color: Colors.grey.shade100,
-                          borderRadius: BorderRadius.circular(8)),
-                      weekendDecoration: BoxDecoration(
-                        color: Colors.red.shade100,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      todayDecoration: const BoxDecoration(
-                        color: Colors.blue,
-                        shape: BoxShape.circle,
-                      ),
-                      selectedDecoration: const BoxDecoration(
-                        color: Colors.blue,
-                        shape: BoxShape.circle,
-                      ),
-                      selectedTextStyle: const TextStyle(color: Colors.white),
+                    );
+                  }
+                  return Center(
+                    child: Text(
+                      text,
+                      style: const TextStyle(fontSize: 18),
                     ),
-                daysOfWeekHeight: 40,
-                headerStyle: widget.headerStyle ??
-                    HeaderStyle(
-                      titleCentered: true,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 4.0,
-                              spreadRadius: 2.0,
-                              color: Colors.grey.shade200,
-                            )
-                          ]),
-                      headerMargin: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 10),
-                      formatButtonVisible: false,
-                      titleTextStyle: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      leftChevronIcon: const Icon(
-                          Icons.arrow_back_ios_new_rounded,
-                          color: Colors.black),
-                      rightChevronIcon: const Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: Colors.black),
-                    ),
-                calendarBuilders: widget.calendarBuilders ??
-                    CalendarBuilders(
-                      dowBuilder: (context, day) {
-                        final text = DateFormat.E().format(day);
-                        if (day.weekday == DateTime.saturday ||
-                            day.weekday == DateTime.sunday) {
-                          return Center(
-                            child: Text(
-                              text,
-                              style: const TextStyle(
-                                  color: Color(0xFFE60026), fontSize: 18),
-                            ),
-                          );
-                        }
-                        return Center(
-                          child: Text(
-                            text,
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                        );
-                      },
-                    ),
-                onPageChanged: widget.onPageChanged ?? (datetime) {},
-              );
-            }),
+                  );
+                },
+              ),
+          onPageChanged: widget.onPageChanged ?? (datetime) {},
+        ),
       ),
     );
   }
