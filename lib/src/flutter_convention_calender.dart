b26004e0f290cfbc57b951c42a7d32f672030dc2
madhusudhan <ghimire.madhusudhan@cloco.net>
2024-09-05 14:44:45 +0545

    confilict solved

--- lib/src/flutter_convention_calender.dart
+++ lib/src/flutter_convention_calender.dart
@@ -1,7 +1,7 @@
+import 'package:convention_calendar/src/custom_dropdown.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:animated_custom_dropdown/custom_dropdown.dart';
 
 import '../utils/app_colors.dart';
 
@@ -71,6 +71,11 @@ class ConventionCalendarState extends State<ConventionCalendar>
     "2029",
     "2030"
   ];
+
+  DateTime _focusedDay = DateTime.now();
+  String? selectedMonth;
+  String? selectedYear;
+
   @override
   void initState() {
     super.initState();
@@ -97,131 +102,143 @@ class ConventionCalendarState extends State<ConventionCalendar>
         curve: Curves.easeInOut,
       ),
     );
+    DateTime now = DateTime.now();
+    selectedMonth = DateFormat('MMMM').format(now);
+    selectedYear = now.year.toString();
   }
-
-  String? selectedMonthsValue;
-  String? selectedYearValue;
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: SafeArea(
-        child: AnimatedBuilder(
-            animation: _animationController,
-            builder: (context, child) {
-              return TableCalendar(
-                firstDay: DateTime(2000),
-                lastDay: DateTime(2025),
-                focusedDay: DateTime.now(),
-                onDaySelected: (selectedDay, focusedDay) {},
-                calendarStyle: CalendarStyle(
-                  defaultTextStyle: const TextStyle(
-                    fontSize: 18,
-                  ),
-                  weekendTextStyle: TextStyle(
-                    color: ColorConstants.error,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18,
-                  ),
-                  defaultDecoration: BoxDecoration(
-                      color: Colors.grey.shade100,
-                      borderRadius: BorderRadius.circular(8)),
-                  weekendDecoration: BoxDecoration(
-                    color: Colors.red.shade100,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  todayDecoration: BoxDecoration(
-                      color: ColorConstants.blue,
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.circular(8)),
-                  selectedDecoration: BoxDecoration(
-                    color: ColorConstants.blue,
-                    shape: BoxShape.circle,
-                  ),
-                  selectedTextStyle: const TextStyle(color: Colors.white),
-                ),
-                daysOfWeekHeight: 40,
-                headerVisible: true,
-                headerStyle: HeaderStyle(
-                  titleCentered: true,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 4.0,
-                          spreadRadius: 2.0,
-                          color: Colors.grey.shade200,
-                        )
-                      ]),
-                  headerMargin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  formatButtonVisible: false,
-                  titleTextStyle: TextStyle(
-                    color: ColorConstants.black800,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  leftChevronIcon: const Icon(Icons.arrow_back_ios_new_rounded,
-                      color: Colors.black),
-                  rightChevronIcon: const Icon(Icons.arrow_forward_ios_rounded,
-                      color: Colors.black),
-                ),
-                calendarBuilders: CalendarBuilders(
-                  headerTitleBuilder: (context, date) => Row(
-                    children: [
-                      Expanded(
-                        flex: 6,
-                        child: CustomDropdown(
-                          initialItem:
-                              DateFormat('MMMM').format(DateTime.now()),
-                          items: months.map((item) => item).toList(),
-                          onChanged: (value) {},
-                          hideSelectedFieldWhenExpanded: false,
-                          overlayHeight: MediaQuery.sizeOf(context).height / 3,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 4,
-                        child: CustomDropdown(
-                          initialItem: DateTime.now().year.toString(),
-                          items: years.map((item) => item).toList(),
-                          onChanged: (value) {},
-                          hideSelectedFieldWhenExpanded: false,
-                          overlayHeight: MediaQuery.sizeOf(context).height / 3,
-                        ),
-                      ),
-                    ],
-                  ),
-                  dowBuilder: (context, day) {
-                    final text = DateFormat.E().format(day);
-                    if (day.weekday == DateTime.saturday ||
-                        day.weekday == DateTime.sunday) {
-                      return Center(
-                        child: Text(
-                          text,
-                          style: const TextStyle(
-                              color: Color(0xFFE60026), fontSize: 18),
-                        ),
-                      );
-                    }
-                    return Center(
-                      child: Text(
-                        text,
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                    );
+      child: TableCalendar(
+        firstDay: DateTime(2000),
+        lastDay: DateTime(2030),
+        focusedDay: _focusedDay,
+        onDaySelected: widget.onDaySelected,
+        calendarStyle: CalendarStyle(
+          defaultTextStyle: const TextStyle(
+            fontSize: 18,
+          ),
+          weekendTextStyle: TextStyle(
+            color: ColorConstants.error,
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+          ),
+          defaultDecoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              borderRadius: BorderRadius.circular(8)),
+          weekendDecoration: BoxDecoration(
+            color: Colors.red.shade100,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          todayTextStyle: const TextStyle(
+              color: Colors.black, fontSize: 16, fontWeight: FontWeight.w500),
+          todayDecoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              border: Border.all(color: ColorConstants.blue, width: 2),
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(8)),
+          selectedDecoration: BoxDecoration(
+            color: ColorConstants.blue,
+            borderRadius: BorderRadius.circular(8),
+            shape: BoxShape.rectangle,
+          ),
+          selectedTextStyle: const TextStyle(color: Colors.white),
+        ),
+        daysOfWeekHeight: 40,
+        headerVisible: true,
+        headerStyle: HeaderStyle(
+          leftChevronMargin: const EdgeInsets.all(0),
+          leftChevronPadding: const EdgeInsets.all(0),
+          rightChevronMargin: const EdgeInsets.all(0),
+          rightChevronPadding: const EdgeInsets.all(0),
+          titleCentered: true,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 4.0,
+                  spreadRadius: 2.0,
+                  color: Colors.grey.shade200,
+                )
+              ]),
+          headerMargin: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+          headerPadding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+          formatButtonVisible: false,
+          leftChevronIcon:
+              const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black),
+          rightChevronIcon:
+              const Icon(Icons.arrow_forward_ios_rounded, color: Colors.black),
+        ),
+        calendarBuilders: CalendarBuilders(
+          headerTitleBuilder: (context, date) => Row(
+            children: [
+              Expanded(
+                flex: 6,
+                child: CustomDropDownWidget(
+                  items: months,
+                  selectedValue: selectedMonth ?? "",
+                  onSelected: (value) {
+                    setState(() {
+                      selectedMonth = value;
+                    });
+                    _updateFocusedDay();
                   },
                 ),
-                onPageChanged: (focusedDay) {
-                  _animationController.forward().then((_) {
-                    _animationController.reverse();
-                  });
-                },
+              ),
+              Expanded(
+                flex: 4,
+                child: CustomDropDownWidget(
+                  items: years,
+                  selectedValue: selectedYear ?? "",
+                  onSelected: (value) {
+                    setState(() {
+                      selectedYear = value;
+                    });
+                    _updateFocusedDay();
+                  },
+                ),
+              ),
+            ],
+          ),
+          dowBuilder: (context, day) {
+            final text = DateFormat.E().format(day);
+            if (day.weekday == DateTime.saturday ||
+                day.weekday == DateTime.sunday) {
+              return Center(
+                child: Text(
+                  text,
+                  style:
+                      const TextStyle(color: Color(0xFFE60026), fontSize: 18),
+                ),
               );
-            }),
+            }
+            return Center(
+              child: Text(
+                text,
+                style: const TextStyle(fontSize: 18),
+              ),
+            );
+          },
+        ),
+        onPageChanged: (focusedDay) {
+          _focusedDay = focusedDay;
+          selectedMonth = DateFormat('MMMM').format(focusedDay);
+          selectedYear = focusedDay.year.toString();
+        },
       ),
     );
+  }
+
+  void _updateFocusedDay() {
+    if (selectedMonth != null && selectedYear != null) {
+      int monthIndex = months.indexOf(selectedMonth!) + 1;
+      DateTime newFocusedDay = DateTime(int.parse(selectedYear!), monthIndex);
+      setState(() {
+        _focusedDay = newFocusedDay;
+      });
+    }
   }
 }
