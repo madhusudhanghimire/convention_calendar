6da2dd58e8c91603e5485f6ca262a117d7147d7b
madhusudhanghimire <ghimire.madhusudhan@cloco.net>
2024-09-09 13:32:25 +0545

    parsed to utc

--- lib/src/flutter_convention_calender.dart
+++ lib/src/flutter_convention_calender.dart
@@ -129,8 +129,8 @@ class ConventionCalendarState extends State<ConventionCalendar>
                 focusedDay: _focusedDay,
                 holidayPredicate: (day) {
                   print(widget.holidays);
-                  print(day);
-                  return widget.holidays?.contains(day) ?? false;
+                  print(day.toUtc());
+                  return widget.holidays?.contains(day.toUtc()) ?? false;
                 },
                 selectedDayPredicate: (day) {
                   return isSameDay(_selectedDay, day);
