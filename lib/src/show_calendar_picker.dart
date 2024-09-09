59c74c5f5aff76ed9bcd8687c9bd6bbcca2ed819
madhusudhanghimire <ghimire.madhusudhan@cloco.net>
2024-09-09 11:12:48 +0545

    class created for convention calendart

--- lib/src/show_calendar_picker.dart
+++ lib/src/show_calendar_picker.dart
@@ -2,67 +2,62 @@ import 'package:convention_calendar/src/flutter_convention_calender.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+class ConventionCalendarPicker {
+  static Future<DateTime?> showConventionCalendarPicker({
+    required BuildContext context,
+    required DateTime firstday,
+    required DateTime lastDay,
+    required DateTime focusedDay,
+    Color? rangeHighLightColor,
+    CalendarStyle? calendarStyle,
+    HeaderStyle? headerStyle,
+    CalendarBuilders? calendarBuilders,
+    List<DateTime>? holidays,
+  }) async {
+    return showDialog<DateTime?>(
+      context: context,
+      builder: (context) {
+        return ConventionCalendar(
+          firstDay: firstday,
+          lastDay: lastDay,
+          focusedDay: focusedDay,
+          calendarStyle: calendarStyle,
+          isRangeEnabled: false,
+          headerStyle: headerStyle,
+          calendarBuilders: calendarBuilders,
+          rangeHighLightColor: rangeHighLightColor,
+          holidays: holidays,
+        );
+      },
+    );
+  }
 
-class ConventionCalendarPicker  {
-
- static Future<DateTime?> showConventionCalendarPicker({
-  required BuildContext context,
-  required DateTime firstday,
-  required DateTime lastDay,
-  required DateTime focusedDay,
-  Color? rangeHighLightColor,
-  CalendarStyle? calendarStyle,
-  HeaderStyle? headerStyle,
-  CalendarBuilders? calendarBuilders,
-  List<DateTime>? holidays,
-}) async {
-  return showDialog<DateTime?>(
-    context: context,
-    builder: (context) {
-      return ConventionCalendar(
-        firstDay: firstday,
-        lastDay: lastDay,
-        focusedDay: focusedDay,
-        calendarStyle: calendarStyle,
-        isRangeEnabled: false,
-        headerStyle: headerStyle,
-        calendarBuilders: calendarBuilders,
-        rangeHighLightColor: rangeHighLightColor,
-        holidays: holidays,
-      );
-    },
-  );
-}
-
-static  Future<DateTime?> showConventionCalendarRangePicker({
-  required BuildContext context,
-  required DateTime firstday,
-  required DateTime lastDay,
-  required DateTime focusedDay,
-  required bool isRangeEnabled,
-  Color? rangeHighLightColor,
-  CalendarStyle? calendarStyle,
-  HeaderStyle? headerStyle,
-  CalendarBuilders? calendarBuilders,
-  List<DateTime>? holidays,
-}) async {
-  return showDialog<DateTime?>(
-    context: context,
-    builder: (context) {
-      return ConventionCalendar.rangeSelection(
-        firstDay: firstday,
-        lastDay: lastDay,
-        focusedDay: focusedDay,
-        isRangeEnabled: isRangeEnabled,
-        calendarStyle: calendarStyle,
-        headerStyle: headerStyle,
-        calendarBuilders: calendarBuilders,
-        rangeHighLightColor: rangeHighLightColor,
-        holidays: holidays,
-      );
-    },
-  );
-}
-
-
+  static Future<DateTime?> showConventionCalendarRangePicker({
+    required BuildContext context,
+    required DateTime firstday,
+    required DateTime lastDay,
+    required DateTime focusedDay,
+    Color? rangeHighLightColor,
+    CalendarStyle? calendarStyle,
+    HeaderStyle? headerStyle,
+    CalendarBuilders? calendarBuilders,
+    List<DateTime>? holidays,
+  }) async {
+    return showDialog<DateTime?>(
+      context: context,
+      builder: (context) {
+        return ConventionCalendar.rangeSelection(
+          firstDay: firstday,
+          lastDay: lastDay,
+          focusedDay: focusedDay,
+          isRangeEnabled: true,
+          calendarStyle: calendarStyle,
+          headerStyle: headerStyle,
+          calendarBuilders: calendarBuilders,
+          rangeHighLightColor: rangeHighLightColor,
+          holidays: holidays,
+        );
+      },
+    );
+  }
 }
